a7a1d4a87c0d18567eb25b09f6fd3164de6b374e
louisdahmna-cmd <louis.dahmna@gmail.com>
2026-02-08 14:38:02 +0100

    Create homepage component in app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+// Homepage component
+import React from 'react';
+
+const Homepage = () => {
+    return (
+        <div>
+            <h1>Welcome to My Backoffice</h1>
+        </div>
+    );
+};
+
+export default Homepage;
